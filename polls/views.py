c35d82ca5c10b0d19c8681e431deae01d8802ae8
courierofcode <notifyemmanuel@gmail.com>
2023-01-26 19:42:33 -0600

    de-hardcoding urls

--- polls/views.py
+++ polls/views.py
@@ -1,29 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from django.template import loader
 
 from .models import Question
-'''
-# Create views with loader
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list
-    }
-    return HttpResponse(template.render(context, request))
-'''
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls\index.html', context)    
+    return render(request, 'polls/index.html', context)    
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except:
-        raise Http404("Question does not exist")
-    return HttpResponse("You're looking at question %s." %question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}    
+    return render(request, 'polls/detail.html', context)
 
 def results(request, question_id):
     return HttpResponse("You're looking at the results of question %s." %question_id)
